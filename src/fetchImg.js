4b7cc90e266fa3aacf55ce3bbaec1986363b4814
Adelina515 <adelgodlevska@gmail.com>
2023-04-29 19:31:50 +0300

    add listener

--- src/fetchImg.js
+++ src/fetchImg.js
@@ -10,20 +10,23 @@
 // }
 
 import axios from "axios";
-
- 
 const API_KEY = "35888485-ef78d2b3836896c1b6f2f0b14";
 const BASE_URL = "https://pixabay.com/api/";
 
-export async function fetchImg() {
+export async function fetchImg(query, page, perPage) {
     try {
      const dataPhoto = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
-            .then((data) => {
-                console.log(data)
+         .then((response) => {
+             if (!response.ok) {
+                throw new Error(response.status);
+             }
+                console.log(response)
             });
         return;
         
-    } catch (error) {
+    } catch (err) {
+         Notify.warning("Sorry, there are no images matching your search query. Please try again.");
+        refs.gallery.innerHTML = '';
         
     }
 }
