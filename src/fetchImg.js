e6b975209e27c172b194d93507a1c739edbfac70
Adelina515 <adelgodlevska@gmail.com>
2023-04-30 12:48:24 +0300

    add css

--- src/fetchImg.js
+++ src/fetchImg.js
@@ -1,32 +1,18 @@
-//  const BASE_URL = "https://restcountries.com/v3.1";
-// export function fetchCountries(name) {
-//     return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-//         .then((response) => {
-//             if (!response.ok) {
-// 			throw new Error(response.status);
-// 		}
-// 		return response.json()
-//         })        
-// }
 
 import axios from "axios";
 const API_KEY = "35888485-ef78d2b3836896c1b6f2f0b14";
 const BASE_URL = "https://pixabay.com/api/";
 
+
+
 export async function fetchImg(query, page, perPage) {
-    try {
-     const dataPhoto = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
-         .then((response) => {
-             if (!response.ok) {
-                throw new Error(response.status);
-             }
-                console.log(response)
-            });
-        return;
-        
-    } catch (err) {
-         Notify.warning("Sorry, there are no images matching your search query. Please try again.");
-        refs.gallery.innerHTML = '';
-        
-    }
+    return await axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
+    inctementPage();
+}
+
+function inctementPage() {
+    page += 1;
+}
+function resetPage() {
+    page = 1;
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,95 @@
 import './css/styles.css';
 import { fetchImg } from "./fetchImg.js";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
+
 
 
 const refs = {
     form: document.getElementById("search-form"),
     gallery: document.querySelector(".gallery"),
+    loadMore: document.querySelector(".load-more"),
 }
 
 refs.form.addEventListener('submit', handleSubmit);
-function handleSubmit(ev) {
-    ev.preventDefault;
-    const inputValue = ev.currentTarget.searchQuery.value;
-    fetchImg(inputValue)
+refs.loadMore.addEventListener('click', handleLoadMore)
+async function handleLoadMore() {
+    refs.loadMore.disabled = true;
+    refs.loadMore.classList.add('visually-hidden')
+    try {
+        return await handleSubmit();
+        if (data.hits.length === data.totalHits) {
+            Notify.warning("We're sorry, but you've reached the end of search results.")
+
+        }
+        refs.loadMore.disabled = false;
+        refs.loadMore.classList.remove('visually-hidden')
+    } catch (err){
+        console.log(err)
+    }   
 }
+async function handleSubmit(ev) {
+    ev.preventDefault();
+    refs.gallery.innerHTML = '';
+     const inputValue = ev.currentTarget.searchQuery.value;
+    try {
+        const { data } = await fetchImg(inputValue, 1, 40); 
+        resetPage();
+        if (data.hits.length === 0) {
+            throw new Error ("No data!")
+        } else {
+            Notify.success(`Hooray! We found ${data.totalHits} imades.`);
+            renderMarkup(data.hits)
+            refs.loadMore.classList.remove('visually-hidden')
+            refs.loadMore.disabled = false;
+        }
+        console.log(data)
+    }
+    catch (err) {
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        refs.gallery.innerHTML = '';
+    }   
+}
+
  
+function renderMarkup(data) {
+    const markup = createGalleryHits(data);
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+    return;
+}
+
+function createGalleryHits(data) {
+  return data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+                    <a class="photo-card__link" href="${largeImageURL}"> <img class="photo-card__img" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>${likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>${views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>${comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>${downloads}
+                         </p>
+                    </div>
+                </div>`;
+      }
+    )
+    .join('');
+}
 
 
  
